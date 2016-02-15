0573cdfd0c771798b31c83c3b39366d232158e6c
Wvsnow <inslwt@163.com>
2016-02-15 16:36:12 +0800

    提交测试。

---
+++ src/main/java/com/ins/spiration/entrance/api/MainTest.java
@@ -0,0 +1,23 @@
+package com.ins.spiration.entrance.api;
+
+/**
+ * MainTest
+ *
+ * @author liwentao1
+ * @Description: ...
+ * @Version 1.0
+ * @See
+ * @time 16:33
+ * @date 2016/2/15
+ */
+public class MainTest {
+
+    public static void main(String[] args){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("++++++++++++++++++++++++++++++++++++++++");
+        }
+    }
+
+}
+
+
